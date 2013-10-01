d97630368ce6838488da767dad1678e349c1116b
Bill Bushey <wbushey@gmail.com>
2013-10-01 11:55:03 -0500

    Moved edit_profile.IHateThisRequirement back go gs.skin.ogn.edem to make Zope happy Made edit_profile PEP8

--- edem/profile/signup/base/edit_profile.py
+++ edem/profile/signup/base/edit_profile.py
@@ -4,7 +4,6 @@ from Products.CustomUserFolder.interfaces import IGSUserInfo
 from Products.Five.browser.pagetemplatefile import \
     ZopeTwoPageTemplateFile
 from Products.GSProfile.profileaudit import ProfileAuditer
-from Products.GSProfile.interfaceSiteProfile import IEDemProfile
 from gs.profile.signup.base.changeprofile import ChangeProfileForm  \
     as RegisterEditProfileForm
 from utils import fn_to_nickname
@@ -12,20 +11,18 @@ from utils import fn_to_nickname
 import logging
 log = logging.getLogger('EDemEditProfileForm')
 
-class IHateThisRequirement(IEDemProfile):
-    '''Placeholder'''
 
 class EDemRegisterEditProfileForm(RegisterEditProfileForm):
     label = u'Change Profile'
     pageTemplateFileName = 'browser/templates/edit_profile_register.pt'
     template = ZopeTwoPageTemplateFile(pageTemplateFileName)
-    
+
     @form.action(label=u'Submit', failure='handle_set_action_failure')
     def handle_set(self, action, data):
         user = self.context
         self.auditer = ProfileAuditer(user)
         self.actual_handle_set(action, data)
-        
+
         userInfo = IGSUserInfo(self.ctx)
         if userInfo.nickname == userInfo.id:
             m = 'Adding nickname to %s (%s)' % (userInfo.name, userInfo.id)
@@ -33,11 +30,11 @@ class EDemRegisterEditProfileForm(RegisterEditProfileForm):
             nickname = fn_to_nickname(user, userInfo.name)
             user.add_nickname(nickname)
         else:
-            nickname = userInfo.nickname # --=mpj17=-- See below
-            
+            nickname = userInfo.nickname  # --=mpj17=-- See below
+
         cf = str(data.pop('came_from'))
         if cf == 'None':
-          cf = ''
+            cf = ''
         if self.user_has_verified_email:
             uri = str(data.get('came_from'))
             if uri == 'None':
@@ -49,7 +46,7 @@ class EDemRegisterEditProfileForm(RegisterEditProfileForm):
             # relational database, and getting it out to form the URI.
             email = self.context.get_emailAddresses()[0]
             uri = '/p/%s/verify_wait.html?form.email=%s&form.came_from=%s' %\
-              (nickname, email, cf)
+                (nickname, email, cf)
         assert uri
         return self.request.RESPONSE.redirect(uri)
 
@@ -60,5 +57,5 @@ class EDemRegisterEditProfileForm(RegisterEditProfileForm):
             print widget.name, omit
             if widget.name not in omit:
                 filtered_widgets.append(widget)
-        
+
         return filtered_widgets
