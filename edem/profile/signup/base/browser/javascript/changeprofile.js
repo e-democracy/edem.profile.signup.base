a3319447054cd872947f95b1b39db369f9d7dd23
Bill Bushey <wbushey@gmail.com>
2014-04-01 17:28:15 -0500

    Tidied up javascript a bit

--- edem/profile/signup/base/browser/javascript/changeprofile.js
+++ edem/profile/signup/base/browser/javascript/changeprofile.js
@@ -1,26 +1,39 @@
-var checkedgroup = [];
-var uncheckedgroup = [];
+jQuery.noConflict();
 
-jQuery("input[name='form.joinable_groups']").each(function(index, element){
-    cbg = jQuery("span[id=checkboxgroup-form-joinable_groups-"+index+"]");
-    if (jQuery(element).is(":checked")) {
-        checkedgroup.push(cbg);
-    } else {
-        uncheckedgroup.push(cbg);
+function edem_changeprofile_init_joinable_groups(){
+    var checkedgroup = [];
+    var uncheckedgroup = [];
+
+    jQuery("input[name='form.joinable_groups']").each(function(index, element){
+        cbg = jQuery("span[id=checkboxgroup-form-joinable_groups-"+index+"]");
+        if (jQuery(element).is(":checked")) {
+            checkedgroup.push(cbg);
+        } else {
+            uncheckedgroup.push(cbg);
+        }
+    });
+
+    jQuery(uncheckedgroup).each(function(index, element) {
+        element.detach();
+        jQuery("#additional-group-fields").append(element);
+    });
+
+    if (jQuery(checkedgroup).length > 0) {
+        jQuery("#missing-groups-message").toggle();
     }
-});
+}
 
-jQuery(uncheckedgroup).each(function(index, element) {
-    element.detach();
-    jQuery("#additional-group-fields").append(element);
-});
+function edem_changeprofile_init_disclosure(){
+    jQuery("#privacy-button").click( function () {
+        var uri = "/support/policies/privacy/ .gs-products-gscontentmanager>:not(#tabmenu)";
+        jQuery("#privacy-content").load(uri);
+    });
 
-if (jQuery(checkedgroup).length > 0) {
-    jQuery("#missing-groups-message").toggle();
 }
         
-jQuery(document).ready( function() {
-    GSPopupFormHelp.init('#edit-profile');
+jQuery(window).ready( function() {
+    edem_changeprofile_init_joinable_groups();
+    edem_changeprofile_init_disclosure();
     GSSuggestFN.init('#form\\.givenName', '#form\\.familyName', '#form\\.fn');
               
     // Handle the interlock between the FN field and the Edit checkbox
